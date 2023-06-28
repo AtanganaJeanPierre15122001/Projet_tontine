1ad2f9ae1b13974062daade07a49edff7bb210e9
TATS03 <steve.tatsambon@facsciences-uy1.cm>
2023-06-28 12:15:20 +0100

    first comit tats

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ session_start();
        
     try 
     {
-        $bdd = new PDO('mysql:host=localhost;port=3307;dbname=tontine','root', 'lemeilleur');
+        $bdd = new PDO('mysql:host=localhost;port=3306;dbname=tontine','root', '');
         $bdd->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         // echo 'good';
     }
